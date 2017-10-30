527dbdf73812a7c76438d8c1b6d3e000dd5df106
KreivenWang <Kreiven.wang@hotmail.com>
2017-10-30 15:02:11 +0800

    添加项目文件。

---
+++ FormReceiver/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace FormReceiver
+{
+    public partial class Form1 : Form
+    {
+        private Thread _thread;
+        private readonly SynchronizationContext _syncContext;
+
+        public Action ReceiveAction => delegate
+        {
+            SetText(label1, "start");
+            var factory = new ConnectionFactory() {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                Port = 6012
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel()) {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    SetText(label1, message);
+                    AddItem(listBox1, message);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+                channel.BasicConsume(queue: "hello",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Thread Sleep");
+                Thread.Sleep(Timeout.Infinite);
+                Console.WriteLine(" END");
+            }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            _syncContext = SynchronizationContext.Current;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //这里选择Thread而不使用Task的原因:
+            //此任务是个监听任务, 需要保持执行, 并可手动停止.
+            //而Task类没有手动停止的功能, 只能等待任务完成.
+            _thread = new Thread(() => { ReceiveAction(); });
+            _thread.Start();
+            button2.Enabled = true;
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _thread.Abort();
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+
+        private void SetText(Control ctrl, string text)
+        {
+            _syncContext.Post(state => ctrl.Text = state.ToString(), text);
+        }
+
+        private void AddItem(ListBox lb, string item)
+        {
+            _syncContext.Post(state => lb.Items.Add(state), item);
+        }
+    }
+}
---
+++ Publisher/Send.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    internal class Send
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++) {
+                GoSend(i);
+                Thread.Sleep(3000);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        public static void GoSend(int s)
+        {
+            var factory = new ConnectionFactory() {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                Port = 6012
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel()) {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!" + s;
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+        }
+    }
+}
